2ba80775f1c6091b52087921b8398e23b34c5324
Cameron Flint <dev@caflint.fastmail.com>
2020-12-25 00:52:17 -0800

    Fix issue with HTML importing & clean up output.

--- README.md
+++ README.md
@@ -7,22 +7,33 @@ A command-line tool to migrate your Journey entries to Day One.
 Features:
 
 * Includes photos
+* Includes tags
 * Includes geocoordinates
 * Includes timestamp (timezone aware)
-* Includes tags
+* Imported text looks as expected (no formatting/encoding issues)
 
 Usage instructions:
 
-1. Export your DayOne journal entries to a .zip file.
-2. Extract the .zip file into a directory.
-3. From a Terminal window, execute the following from the root of the repository:
+1. Clone & restore dependencies in this repository.
+1. Make sure the `dayone2` executable is on your PATH (select DayOne > Install Command Line Tools...).
+1. Export your Journey entries to a .zip file.
+1. Extract the .zip file into a directory.
+1. From a Terminal window, execute the following from the root of the repository:
 
 ```shell
 $ python3 j2d.py <name-of-target-journal> <path-to-unzipped-export-directory>
-Added: 1536079037061-o0c7k9jp94bdssws -> 08FF91BA31074546847946D066E652AA
-Added: 1524500154963-4tjvzwyoaltaoo6i -> 9A7177F4B56541E6AAEF6E3FBDC5BFD9
+WARNING: coordinates are invalid: 1.7976931348623157e+308 1.7976931348623157e+308
+[1/508] Added new: 1532920577071-bxzfmf02n8r96vb5 -> 151FDBF1CB1B4F658C261131AD7296D4: 66 words, 8 tags, 6 photos
+[2/508] Added new: 1590644431033-3b8kodxeyhfwatze -> 5766077480B3478AA5744906752B7E9A: 9 words
+[3/508] Added new: 1549735900606-555qqtpidpf8zjne -> 661865B88EC441599645B1FBCB3FD5C8: 12 words, 2 tags
+[4/508] Added new: 1525981267299-j6iz9i1ajnkg4ofw -> 5937EA89A1794410975E8C22EEB65EFD: 10 words, 3 tags
+[5/508] Added new: 1513562853667-3fc7acbe265c58d0 -> 9D743D63A097476694BA297FA4FFA80D: 12 words, 4 tags, 2 photos
+[6/508] Added new: 1590649634456-q8d3kalowuq19lx7 -> A395946A089B4D3DA4D33553E09D73C5: 4 tags, 1 photos
+[7/508] Added new: 1515011399977-3fdb529186e4fc32 -> 6717268DB7C24B678A7E8BFB0F2A9615: 29 words, 3 tags
+[8/508] Added new: 1529389558714-1r4s6scsr96di5yq -> 913F75A728154FCBA1D8D50DC94B5E98: 25 words, 6 tags, 3 photos
+[9/508] Added new: 1538105531793-846iyuspmeh91svn -> BC1F204F2749437A8E6AC82A6869A02D: 2 words, 4 tags, 3 photos
 ...
 
-621 succeeded, 0 failed, 4 skipped
+508 succeeded, 0 failed, 1 skipped
 ```
 
--- j2d.py
+++ j2d.py
@@ -7,8 +7,9 @@ import subprocess
 from dataclasses import dataclass
 from datetime import datetime
 from pathlib import Path
-from typing import List, Iterable
+from typing import List, Iterable, Optional, Tuple
 
+from bs4 import BeautifulSoup
 from pytz import timezone as tz, UnknownTimeZoneError
 from tzlocal import get_localzone
 
@@ -29,10 +30,31 @@ class JourneyEntry:
 
 
 @dataclass
-class ImporterStatistics:
-    succeeded: int = 0
-    failed: int = 0
-    skipped: int = 0
+class ValidatedEntry:
+    foreign_id: str
+    source_path: str
+    text: str
+    tags: List[str]
+    photos: List[str]
+    lat: Optional[float]
+    lon: Optional[float]
+    timestamp: str
+    timezone: str
+
+
+@dataclass
+class ImportOneResult:
+    image_count: int = 0
+    tag_count: int = 0
+    word_count: int = 0
+
+
+@dataclass
+class ImportManyResult:
+    failed_paths: List[str]
+    skipped_paths: List[str]
+    attempted_count: int = 0
+    total_count: int = 0
 
 
 def parse_args():
@@ -47,125 +69,201 @@ def parse_args():
 
 
 class Importer:
-    def __init__(self, src_directory, target_journal_name):
+    def __init__(self, src_directory, target_journal_name, debug=False):
         self.src_directory = src_directory
         self.target_journal_name = target_journal_name
-        self.retryList = []
-        self.stats = ImporterStatistics()
+        self.debug = debug
+        self.data = None
 
     def run(self):
-        entries = self.load_journey_entries(self.src_directory)
-        self.import_entries(entries)
-        self.print_stats()
+        self.data = ImportManyResult([], [])
+        raw_entries = self.load_journey_entries(self.src_directory)
+        valid_entries = self.validate_journey_entries(raw_entries)
+        imported_entries = self.import_entries(valid_entries)
+        self.print_result(imported_entries)
 
-    def load_journey_entries(self, root):
+    def load_journey_entries(self, root) -> Iterable[JourneyEntry]:
         for file in self.iter_journey_files(root):
             yield self.load_entry(file)
 
-    def iter_journey_files(self, root):
+    def iter_journey_files(self, root) -> Iterable[str]:
         p = Path(root)
         for file in p.rglob('*.json'):
             yield file
 
-    def load_entry(self, file):
+    def load_entry(self, file) -> JourneyEntry:
         with open(file) as f:
             text = f.read()
             body = json.loads(text)
             entry = self.extract_entry_from_body(body, file)
             return entry
 
-    def extract_entry_from_body(self, body, path):
+    def extract_entry_from_body(self, body, path) -> JourneyEntry:
         return JourneyEntry(id=body['id'], date_journal=body['date_journal'], text=body['text'], lon=body['lon'],
                             lat=body['lat'], tags=body['tags'], photos=body['photos'], address=body['address'],
                             type=body['type'], timezone=body['timezone'], path=path)
 
-    def import_entries(self, entries: Iterable[JourneyEntry]):
-        for entry in entries:
-            if not len(entry.text) > 0:
-                self.stats.skipped += 1
-                continue
-            id, err = self.import_one_entry(entry)
-            if not err:
-                self.stats.succeeded += 1
-                print('Added: {} -> {}'.format(entry.id, id))
-            else:
-                self.stats.failed += 1
-                self.retryList.append(entry.path)
-                print('ERROR: {}'.format(err))
+    def validate_journey_entries(self, raw_entries: Iterable[JourneyEntry]) -> Iterable[ValidatedEntry]:
+        for raw in raw_entries:
+            entry = self.build_valid_entry(raw)
+            if entry:
+                yield entry
 
-    def import_one_entry(self, entry: JourneyEntry):
-        id = ''
-        err = ''
-        args = self.build_dayone_args(entry)
-        print(args)
-        p = subprocess.run(args, input=entry.text, text=True, capture_output=True)
-        if p.returncode == 0:
-            # Parse entry ID. E.g.: Created new entry with uuid: CB17A357BED34F6D838410CA96C7D9D1
-            m = re.search(r'([A-F0-9]+)\s*$', p.stdout)
-            if m:
-                id = m.group(1)
-        else:
-            err = p.stderr
-        return id, err
-
-    def build_dayone_args(self, entry: JourneyEntry):
-        args = ['dayone2', '-j', self.target_journal_name]
+    def build_valid_entry(self, raw: JourneyEntry) -> Optional[ValidatedEntry]:
+        foreign_id = raw.id
+        source_path = raw.path
 
         timezone = ''
-        if entry.timezone:
+        if raw.timezone:
             try:
-                timezone = tz(entry.timezone).zone
+                timezone = tz(raw.timezone).zone
             except UnknownTimeZoneError:
-                print('WARNING: timezone is invalid: {}'.format(entry.timezone))
+                print('WARNING: timezone is invalid: {}'.format(raw.timezone))
         if not timezone:
             timezone = get_localzone().zone
-        args.extend(['-z', timezone])
 
         timestamp = ''
         timestamp_format = '%Y-%m-%d %I:%M:%S %p'
-        if entry.date_journal is not None:
+        if raw.date_journal is not None:
             try:
-                dt = datetime.fromtimestamp(entry.date_journal / 1000)
+                dt = datetime.fromtimestamp(raw.date_journal / 1000)
                 timestamp = dt.strftime(timestamp_format)
             except (OverflowError, ValueError, OSError):
-                print('WARNING: timestamp is invalid: {}'.format(entry.date_journal))
+                print('WARNING: timestamp is invalid: {}'.format(raw.date_journal))
         if not timestamp:
             dt = datetime.now(tz(timezone))
             timestamp = dt.strftime(timestamp_format)
-        args.extend(['-d', timestamp])
 
-        if entry.lat is not None and entry.lon is not None:
-            is_lat_valid = math.isfinite(entry.lat) and math.fabs(entry.lat) <= 90
-            is_lon_valid = math.isfinite(entry.lon) and math.fabs(entry.lon) <= 180
-            if is_lat_valid and is_lon_valid:
-                args.extend(['--coordinate', str(entry.lat), str(entry.lon)])
-            else:
-                print('WARNING: coordinates are invalid: {} {}'.format(entry.lat, entry.lon))
-
-        if len(entry.tags) > 0:
-            tag_names = map(lambda t: re.sub(r'\s+', r'\\\g<0>', t), entry.tags)
-            args.extend(['-t', *tag_names])
-
-        if len(entry.photos) > 0:
-            valid_paths = []
-            for path in entry.photos:
+        photos = []
+        if len(raw.photos) > 0:
+            for path in raw.photos:
                 abs_path = os.path.abspath(os.path.join(self.src_directory, path))
                 if os.path.exists(abs_path) and os.path.isfile(abs_path):
-                    valid_paths.append(abs_path)
+                    photos.append(abs_path)
                 else:
                     print('WARNING: photo path is invalid: {}'.format(abs_path))
-            args.extend(['-p', *valid_paths])
 
+        tags = []
+        if len(raw.tags) > 0:
+            escaped_tags = map(self.escape_tag, raw.tags)
+            tags.extend(escaped_tags)
+
+        lat = None
+        lon = None
+        if raw.lat is not None and raw.lon is not None:
+            is_lat_valid = math.isfinite(raw.lat) and math.fabs(raw.lat) <= 90
+            is_lon_valid = math.isfinite(raw.lon) and math.fabs(raw.lon) <= 180
+            if is_lat_valid and is_lon_valid:
+                lat = raw.lat
+                lon = raw.lon
+            else:
+                print('WARNING: coordinates are invalid: {} {}'.format(raw.lat, raw.lon))
+
+        skip = False
+        text = self.strip_text_from_html_body(raw.text)
+        if not text:
+            print('WARNING: entry has no text: id={}'.format(raw.id))
+        if not text and len(photos) == 0:
+            print('WARNING: entry has no text and no photos, skipping: id={}'.format(raw.id))
+            skip = True
+        if text.find('dayone-moment:') != -1:
+            print('WARNING: skipped previously-exported DayOne entry: id={}'.format(raw.id))
+            skip = True
+
+        if not skip:
+            return ValidatedEntry(foreign_id=foreign_id, source_path=source_path, text=text, photos=photos, tags=tags,
+                                  lat=lat, lon=lon, timestamp=timestamp, timezone=timezone)
+        else:
+            self.data.skipped_paths.append(source_path)
+            return None
+
+    def escape_tag(self, raw):
+        tag = re.sub(r'\s+', r'\\\g<0>', raw)
+        return tag
+
+    def strip_text_from_html_body(self, original_text):
+        soup = BeautifulSoup(original_text, 'html5lib')
+        is_html = bool(soup.find())  # true if at least one HTML element can be found
+        if is_html:
+            return soup.get_text(strip=True)
+        else:
+            return original_text
+
+    def import_entries(self, entries: Iterable[ValidatedEntry]):
+        flat_entries = list(entries)
+        self.data.total_count = len(flat_entries)
+        for entry in flat_entries:
+            id, err = self.import_one_entry(entry)
+            self.data.attempted_count += 1
+            if not err:
+                prefix = '[{}/{}]'.format(self.data.attempted_count, self.data.total_count)
+                data = []
+                if entry.text:
+                    data.append('{} words'.format(len(entry.text.split())))
+                if len(entry.tags):
+                    data.append('{} tags'.format(len(entry.tags)))
+                if len(entry.photos):
+                    data.append('{} photos'.format(len(entry.photos)))
+                if len(data):
+                    print('{} Added new: {} -> {}: {}'.format(prefix, entry.foreign_id, id, ', '.join(data)))
+                else:
+                    print('{} Added new: {} -> {}'.format(prefix, entry.foreign_id, id))
+                yield entry
+            else:
+                self.data.failed_paths.append(entry.source_path)
+                print('ERROR: {}'.format(err))
+
+    def import_one_entry(self, entry: ValidatedEntry) -> Tuple[Optional[str], Optional[str]]: # id, err
+        args = self.build_dayone_args(entry)
+        if self.debug:
+            print(args)
+        p = subprocess.run(args, input=entry.text, text=True, capture_output=True)
+        if p.returncode == 0:
+            id = self.parse_id_from_output(p.stdout)
+            return id, None
+        else:
+            err = p.stderr
+            return None, err
+
+    def build_dayone_args(self, entry: ValidatedEntry):
+        args = ['dayone2', '-j', self.target_journal_name]
+        args.extend(['-d', entry.timestamp])
+        args.extend(['-z', entry.timezone])
+        if len(entry.tags) > 0:
+            args.extend(['-t', *entry.tags])
+        if len(entry.photos) > 0:
+            args.extend(['-p', *entry.photos])
+        if entry.lat and entry.lon:
+            args.extend(['--coordinate', str(entry.lat), str(entry.lon)])
         args.extend(['--', 'new'])
-
         return args
 
-    def print_stats(self):
+    def parse_id_from_output(self, output):
+        # E.g.: "Created new entry with uuid: CB17A357BED34F6D838410CA96C7D9D1"
+        m = re.search(r'([A-F0-9]+)\s*$', output)
+        if m:
+            id = m.group(1)
+            return id
+        else:
+            return ''
+
+    def print_result(self, imported_entries: Iterable[ValidatedEntry]):
+        succeeded_count = len(list(imported_entries))
+        self.print_paths("SKIPPED", self.data.skipped_paths)
+        self.print_paths("FAILED", self.data.failed_paths)
+        skipped_count = len(self.data.skipped_paths)
+        failed_count = len(self.data.failed_paths)
         print()
-        print('{} succeeded, {} failed, {} skipped'.format(self.stats.succeeded, self.stats.failed, self.stats.skipped))
+        print('{} succeeded, {} failed, {} skipped'.format(succeeded_count, failed_count, skipped_count))
+
+    def print_paths(self, prefix, paths):
+        if len(paths):
+            print()
+        for path in paths:
+            print('{}: {}'.format(prefix, path))
 
 
 if __name__ == '__main__':
     target_journal_name, src_directory = parse_args()
-    i = Importer(src_directory, target_journal_name)
+    i = Importer(src_directory, target_journal_name, debug=False)
     i.run()
